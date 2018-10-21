ae487519903f415cc179a0d003c10375d8cd87af
Ruben <rubenvelaaliaga@gmail.com>
2018-10-21 13:23:29 +0200

    FormularioParte2,Terminado

--- Ejercicio13/javascript/main.js
+++ Ejercicio13/javascript/main.js
@@ -4,25 +4,41 @@ function onSubmit(form){
     console.log( data );
     return false; //don't submit
   }
+function MostrarGreu(){
 
-  function Ayuda(){
-      
-      alert("a.- Les faltes de  puntualitat injustificades. \n \n b.- Les faltes d'assistència injustificades. \n \n c.- Els actes que alteren el normal desemvolupament"
-      +"de les activitats del centre educatiu, especialment els que alteren el normal desemvolupament de les classes. \n \n d.Els actes d'indisciplina. \n \n"+
-      "e.- Els actes d'incorrecció o desconsideració, les injúries i les ofenses contra el membres de la comunitat educativa. \n \n"+
-      "f.- El furt o deteriorament intencionat d'inmobles,materials,documentació o recursos del centre.\n \n"+
-      "g.- El furt o deteriorament intencionat dels béns o materials  dels membres de la comunitat educativa \n \n"+
-      "h.- Les accions que puguen ser perjudicials per a la integritat i la salut dels membres de la comunitat educativa. \n \n"+
-      "i.- La negativa sisteàtica a portar el material necessari per al desenvolupament del procés d'ensenyament-aprenentatge. \n \n"+
-      "j.- La negativa a traslladar la informació facilitada als pares, mares, tutors, o tutores per part del centre i viceversa. \n \n"+
-      "k.- L'alteració o manipulació de la documentació facilitada als ares, mares, tutors o tutores per part del centre \n \n"+
-        "l.- La suplantació de la personalitat dels membres de la comunitat escolar \n \n"+
-        "m.- La utilizació inadequada de les tecnologies de la informació i comunicació durant les activitats que es realitzen en el centre educatiu \n \n"+
-      "n.- L'us de telèfons mòbils, aparells de so i altres aparells electrònics aliens al procés d'ensenyament-aprenentatge. Durant les activitats que es realitzen en el centre educatiu. \n \n"+
-      "o.- Els actes que dificulten o impedisquen el dret i el deure de l'estudi dels seus companys i companyes. \n \n "+
-      "p.- La incitació o estimul a cometre una falta contrària a les normes de convivència. \n \n"+
-      "q.- La negativa al compliment de les mesures correctores adoptades davant de conductes contràries a les normes de convivència. \n \n"+
-      "r.- L'us inadequat de les infrastructures i béns o equips materials del centre. \n \n"+
-      "s.- La desobediència en el compliment de les normes de carácter propi del centre i que estiguen incloses en el seu projecte educatiu.");
+  document.getElementById("Grave").classList.add("MostrarGrave");
+  document.getElementById("Grave").classList.remove("QuitarGrave");
+  document.getElementById("Leve").classList.add("QuitarLeve");
 
+
+
+}
+function MostrarLeve(){
+
+  document.getElementById("Grave").classList.remove("MostrarGrave");
+  document.getElementById("Grave").classList.add("QuitarGrave");
+  document.getElementById("Leve").classList.remove("QuitarLeve");
+
+
+
+}
+
+window.onload = function(){
+  var fecha = new Date(); //Fecha actual
+  var mes = fecha.getMonth()+1; //obteniendo mes
+  var dia = fecha.getDate(); //obteniendo dia
+  var anyo = fecha.getFullYear(); //obteniendo año
+  if(dia<10){
+    dia='0'+dia; //agrega cero si el menor de 10
   }
+  if(mes<10){
+    mes='0'+mes //agrega cero si el menor de 10
+  }
+
+  document.getElementById('fechaIncidente').value= anyo+"-"+mes+"-"+dia;
+  document.getElementById('fechaAtencion').value= anyo+"-"+mes+"-"+dia;
+  document.getElementById('ExtraescolaresIni').value= anyo+"-"+mes+"-"+dia;
+  document.getElementById('SuspensionIni').value= anyo+"-"+mes+"-"+dia;
+
+}
+  
