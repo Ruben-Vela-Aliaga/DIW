6094998c4b5c50bab2df41cfc5a3043e09e290fe
Ruben-Vela-Aliaga <rubenvelaaliaga@gmail.com>
2018-10-22 17:29:55 +0200

    Terminar ocultar y mostrar los checks

--- Ejercicio13/javascript/main.js
+++ Ejercicio13/javascript/main.js
@@ -13,6 +13,35 @@ function MostrarGreu(){
 
 
 }
+function MostrarD(){
+  if( document.getElementById("checkBox-4").checked == 1){
+    document.getElementById("isCheckedD").classList.add("mostrarD");
+    document.getElementById("isCheckedD").classList.remove("quitarD");
+  }else{
+    document.getElementById("isCheckedD").classList.remove("mostrarD");
+    document.getElementById("isCheckedD").classList.add("quitarD");
+  } 
+  
+  
+}
+function MostrarE(){
+  if( document.getElementById("checkBox-5").checked == 1){
+    document.getElementById("isCheckedE").classList.add("mostrarD");
+    document.getElementById("isCheckedE").classList.remove("quitarD");
+  }else{
+    document.getElementById("isCheckedE").classList.remove("mostrarD");
+    document.getElementById("isCheckedE").classList.add("quitarD");
+  }
+}
+function MostrarF(){
+  if( document.getElementById("checkBox-4").checked == 1){
+    document.getElementById("isChecked").classList.add("mostrarD");
+    document.getElementById("isChecked").classList.remove("quitarD");
+  }else{
+    document.getElementById("isChecked").classList.remove("mostrarD");
+    document.getElementById("isChecked").classList.add("quitarD");
+  }
+}
 function MostrarLeve(){
 
   document.getElementById("Grave").classList.remove("MostrarGrave");
@@ -22,8 +51,8 @@ function MostrarLeve(){
 
 
 }
-
-window.onload = function(){
+function inicio(){
+  console.log("hola");
   var fecha = new Date(); //Fecha actual
   var mes = fecha.getMonth()+1; //obteniendo mes
   var dia = fecha.getDate(); //obteniendo dia
@@ -39,6 +68,7 @@ window.onload = function(){
   document.getElementById('fechaAtencion').value= anyo+"-"+mes+"-"+dia;
   document.getElementById('ExtraescolaresIni').value= anyo+"-"+mes+"-"+dia;
   document.getElementById('SuspensionIni').value= anyo+"-"+mes+"-"+dia;
+  document.getElementById('DocumentoLeve').checked = 1;
 
 }
   
