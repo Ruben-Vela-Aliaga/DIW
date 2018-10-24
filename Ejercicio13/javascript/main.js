f1e394f189a48c917788ab0508f3e508a1e89630
Ruben-Vela-Aliaga <rubenvelaaliaga@gmail.com>
2018-10-24 19:01:31 +0200

    Finalizado

--- Ejercicio13/javascript/main.js
+++ Ejercicio13/javascript/main.js
@@ -1,10 +1,17 @@
-function onSubmit(form){
-    
-    var data = JSON.stringify( $(form).serializeArray() ); //  <-----------
-    console.log( data );
-    return false; //don't submit
-  }
-function MostrarGreu(){
+
+
+function onSubmit(form) {
+
+  var data = JSON.stringify($(form).serializeArray()); //  <-----------
+  console.log(data);
+  return false; //don't submit
+
+}
+function regargar() {
+
+  window.location.reload(true);
+}
+function MostrarGreu() {
 
   document.getElementById("Grave").classList.add("MostrarGrave");
   document.getElementById("Grave").classList.remove("QuitarGrave");
@@ -13,36 +20,19 @@ function MostrarGreu(){
 
 
 }
-function MostrarD(){
-  if( document.getElementById("checkBox-4").checked == 1){
-    document.getElementById("isCheckedD").classList.add("mostrarD");
-    document.getElementById("isCheckedD").classList.remove("quitarD");
-  }else{
-    document.getElementById("isCheckedD").classList.remove("mostrarD");
-    document.getElementById("isCheckedD").classList.add("quitarD");
-  } 
-  
-  
-}
-function MostrarE(){
-  if( document.getElementById("checkBox-5").checked == 1){
-    document.getElementById("isCheckedE").classList.add("mostrarD");
-    document.getElementById("isCheckedE").classList.remove("quitarD");
-  }else{
-    document.getElementById("isCheckedE").classList.remove("mostrarD");
-    document.getElementById("isCheckedE").classList.add("quitarD");
+function Mostrar(idCheck, id) {
+  if (document.getElementById(idCheck).checked == 1) {
+    document.getElementById(id).classList.add("mostrarD");
+    document.getElementById(id).classList.remove("quitarD");
+  } else {
+    document.getElementById(id).classList.remove("mostrarD");
+    document.getElementById(id).classList.add("quitarD");
   }
+
+
 }
-function MostrarF(){
-  if( document.getElementById("checkBox-4").checked == 1){
-    document.getElementById("isChecked").classList.add("mostrarD");
-    document.getElementById("isChecked").classList.remove("quitarD");
-  }else{
-    document.getElementById("isChecked").classList.remove("mostrarD");
-    document.getElementById("isChecked").classList.add("quitarD");
-  }
-}
-function MostrarLeve(){
+
+function MostrarLeve() {
 
   document.getElementById("Grave").classList.remove("MostrarGrave");
   document.getElementById("Grave").classList.add("QuitarGrave");
@@ -51,24 +41,22 @@ function MostrarLeve(){
 
 
 }
-function inicio(){
-  console.log("hola");
+function inicio() {
   var fecha = new Date(); //Fecha actual
-  var mes = fecha.getMonth()+1; //obteniendo mes
+  var mes = fecha.getMonth() + 1; //obteniendo mes
   var dia = fecha.getDate(); //obteniendo dia
   var anyo = fecha.getFullYear(); //obteniendo año
-  if(dia<10){
-    dia='0'+dia; //agrega cero si el menor de 10
+  if (dia < 10) {
+    dia = '0' + dia; //agrega cero si el menor de 10
   }
-  if(mes<10){
-    mes='0'+mes //agrega cero si el menor de 10
+  if (mes < 10) {
+    mes = '0' + mes //agrega cero si el menor de 10
   }
 
-  document.getElementById('fechaIncidente').value= anyo+"-"+mes+"-"+dia;
-  document.getElementById('fechaAtencion').value= anyo+"-"+mes+"-"+dia;
-  document.getElementById('ExtraescolaresIni').value= anyo+"-"+mes+"-"+dia;
-  document.getElementById('SuspensionIni').value= anyo+"-"+mes+"-"+dia;
+  document.getElementById('fechaIncidente').value = anyo + "-" + mes + "-" + dia;
+  document.getElementById('fechaAtencion').value = anyo + "-" + mes + "-" + dia;
+  document.getElementById('ExtraescolaresIni').value = anyo + "-" + mes + "-" + dia;
+  document.getElementById('SuspensionIni').value = anyo + "-" + mes + "-" + dia;
   document.getElementById('DocumentoLeve').checked = 1;
 
 }
-  
