21cd04cc3c94eb4cc563b3e73c304ed55f3d3115
Ruben-Vela-Aliaga <rubenvelaaliaga>
2018-11-26 23:19:50 +0100

    Implantar el icono editar en las demas pag menos listarLeves

--- Ejercicio13/formulario-servidor2.0/Cliente/javascript/listarLeves.js
+++ Ejercicio13/formulario-servidor2.0/Cliente/javascript/listarLeves.js
@@ -2,19 +2,21 @@ var Delete="<span class='badge badge-danger'>Eliminada</span>";
 $.getJSON( "faltas/faltasLeves", function( data ) {
   var items = [];
     $.each( data, function( key, val ) {
+      
     if( val.Eliminada == true){
       if(val.nombreAlumno== ""){
         val.nombreAlumno="Sin nombre";
-      items.push( "<li id='" + key + "' class='list-group-item' ><b>Alumno :</b> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a>"+Delete+"</li>" );
+      items.push( "<li id='" + key + "' class='list-group-item' ><b>Alumno :</b> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a>"+Delete+"</li>" );
+    
     }else{
-      items.push( "<li id='" + key + "' class='list-group-item' ><b>Alumno :</b> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a>"+Delete+"</li>" );
+      items.push( "<li id='" + key + "' class='list-group-item' ><b>Alumno :</b> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a>"+Delete+"</li>" );
     }
     }else{
       if(val.nombreAlumno== ""){
         val.nombreAlumno="Sin nombre";
-      items.push( "<li id='" + key + "' class='list-group-item' ><b>Alumno :</b> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></li>" );
+      items.push( "<li id='" + key + "' class='list-group-item' ><b>Alumno :</b> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></li>" );
     }else{
-      items.push( "<li id='" + key + "' class='list-group-item' ><b>Alumno :</b> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></li>" );
+      items.push( "<li id='" + key + "' class='list-group-item' ><b>Alumno :</b> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></li>" );
     }
   }
     
@@ -40,6 +42,18 @@ $.getJSON( "faltas/faltasLeves", function( data ) {
 
 
 
+function Editar(img){
+  var padre= document.getElementById(img.id).parentNode;
+ 
+  var Hijo= document.getElementById(padre.id).childNodes;
+
+ var idFalta=Hijo[2].href;
+
+ document.getElementById("Editar"+padre.id).href=idFalta;
 
 
- document.addEventListener("DOMContentLoaded",cargar);
+
+ 
+
+}
+
