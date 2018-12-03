51595df59ca7e693a885f9d808cf267507ad282d
Ruben-Vela-Aliaga <rubenvelaaliaga@gmail.com>
2018-12-03 17:24:21 +0100

    Terminado CSS

--- Incidencias/Cliente/javascript/listarBorradas.js
+++ Incidencias/Cliente/javascript/listarBorradas.js
@@ -5,27 +5,29 @@ $.getJSON( "faltas/faltasBorradas", function( data ) {
     $.each( data, function( key, val ) {
       var fecha=ponerFechabien(val.fechaIncidente);
       if(cabecera==false){
-        items.push( "<tr><th>Editar</th><th>Alumno</th><th>Profesor</th><th>Fecha de la Falta</th><th>Estado</th></tr>" );
+        items.push( "<tr><th>Alumno</th><th>Profesor</th><th>Fecha de la Falta</th><th>Estado</th><th>Editar</th></tr>" );
         cabecera=true;
       }
     if( val.Eliminada == true){
       if(val.nombreAlumno== ""){
         val.nombreAlumno="Sin nombre";
         
-      items.push( "<tr id='"+key+"'><td><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></td> id='" + key + "' class='list-group-item' ><td> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></td><td>"+val.nombreProfessor+"</td><td>"+fecha+"</td><td>"+Delete+"</td></tr>" );
+      items.push( "<tr id='"+key+"'> id='" + key + "' class='list-group-item' ><td> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></td><td>"+val.nombreProfessor+"</td><td>"+fecha+"</td><td>"+Delete+"</td><td><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></td> </tr>" );
     
     }else{
-      items.push( "<tr id='"+key+"'><td><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></td> id='" + key + "' class='list-group-item' ><td> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></td><td>"+val.nombreProfessor+"</td><td>"+fecha+"</td><td>"+Delete+"</td></tr>" );
+      items.push( "<tr id='"+key+"'>id='" + key + "' class='list-group-item' ><td> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></td><td>"+val.nombreProfessor+"</td><td>"+fecha+"</td><td>"+Delete+"</td><td><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></td> </tr>" );
     }
     }else{
       if(val.nombreAlumno== ""){
         val.nombreAlumno="Sin nombre";
-      items.push( "<tr id='"+key+"'><td><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></td> id='" + key + "' class='list-group-item' ><td> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></td><td>"+val.nombreProfessor+"</td><td>"+fecha+"</td><td></td></tr>" );
+      items.push( "<tr id='"+key+"'> id='" + key + "' class='list-group-item' ><td> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></td><td>"+val.nombreProfessor+"</td><td>"+fecha+"</td><td></td><td><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></td> </tr>" );
     }else{
-      items.push( "<tr id='"+key+"'><td><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></td> id='" + key + "' class='list-group-item' ><td><a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></td><td>"+val.nombreProfessor+"</td><td>"+fecha+"</td><td></td></tr>" );
+      items.push( "<tr id='"+key+"'>id='" + key + "' class='list-group-item' ><td><a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></td><td>"+val.nombreProfessor+"</td><td>"+fecha+"</td><td></td><td><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></td> </tr>" );
     }
   }
     
+  
+     
   
      
       
@@ -62,13 +64,18 @@ function ponerFechabien(fecha){
 function Editar(img){
   //Cojo el abuelo del enlace donde tengo el evento onclick
   var padre= img.parentNode;
+
   var abuelo=padre.parentNode;
+  
  //listo los hijos que tiene  el abuelo
   var HijoAbuelo= document.getElementById(abuelo.id).childNodes;
    //listo los hijos que tiene  el segundo td que es donde esta el enlace
-  var hijoPadre=HijoAbuelo[1].childNodes;
+  var hijoPadre=HijoAbuelo[0].childNodes;
  //cojo el enlace
+
  var Enlace=hijoPadre[1].href;
+ 
+
  //luego pongo dicha ruta en el enlace donde esta el evento onlclick
  document.getElementById("Editar"+abuelo.id).href=Enlace;
 
--- Incidencias/Cliente/javascript/listarGraves.js
+++ Incidencias/Cliente/javascript/listarGraves.js
@@ -6,25 +6,29 @@ $.getJSON( "faltas/faltasGraves", function( data ) {
   console.log(key);
   var fecha=ponerFechabien(val.fechaIncidente);
   if(cabecera==false){
-    items.push( "<tr><th>Editar</th><th>Alumno</th><th>Profesor</th><th>Fecha de la Falta</th><th>Estado</th></tr>" );
+    items.push( "<tr><th>Alumno</th><th>Profesor</th><th>Fecha de la Falta</th><th>Estado</th><th>Editar</th></tr>" );
     cabecera=true;
   }
-  if( val.Eliminada == true){
-    if(val.nombreAlumno== ""){
-      val.nombreAlumno="Sin nombre";
-      items.push( "<tr id='"+key+"'><td><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></td> id='" + key + "' class='list-group-item' ><td> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></td><td>"+val.nombreProfessor+"</td><td>"+fecha+"</td><td>"+Delete+"</td></tr>" );
-  
-  }else{
-    items.push( "<tr id='"+key+"'><td><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></td> id='" + key + "' class='list-group-item' ><td> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></td><td>"+val.nombreProfessor+"</td><td>"+fecha+"</td><td>"+Delete+"</td></tr>" );
-  }
-  }else{
-    if(val.nombreAlumno== ""){
-      val.nombreAlumno="Sin nombre";
-      items.push( "<tr id='"+key+"'><td><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></td> id='" + key + "' class='list-group-item' ><td> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></td><td>"+val.nombreProfessor+"</td><td>"+fecha+"</td><td></td></tr>" );
-  }else{
-    items.push( "<tr id='"+key+"'><td><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></td> id='" + key + "' class='list-group-item' ><td> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></td><td>"+val.nombreProfessor+"</td><td>"+fecha+"</td><td></td></tr>" );
-  }
+if( val.Eliminada == true){
+  if(val.nombreAlumno== ""){
+    val.nombreAlumno="Sin nombre";
+    
+  items.push( "<tr id='"+key+"'> id='" + key + "' class='list-group-item' ><td> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></td><td>"+val.nombreProfessor+"</td><td>"+fecha+"</td><td>"+Delete+"</td><td><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></td> </tr>" );
+
+}else{
+  items.push( "<tr id='"+key+"'>id='" + key + "' class='list-group-item' ><td> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></td><td>"+val.nombreProfessor+"</td><td>"+fecha+"</td><td>"+Delete+"</td><td><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></td> </tr>" );
 }
+}else{
+  if(val.nombreAlumno== ""){
+    val.nombreAlumno="Sin nombre";
+  items.push( "<tr id='"+key+"'> id='" + key + "' class='list-group-item' ><td> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></td><td>"+val.nombreProfessor+"</td><td>"+fecha+"</td><td></td><td><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></td> </tr>" );
+}else{
+  items.push( "<tr id='"+key+"'>id='" + key + "' class='list-group-item' ><td> <a href='detalles.html?id="+val._id+"'>" + val.nombreAlumno + "</a></td><td>"+val.nombreProfessor+"</td><td>"+fecha+"</td><td></td><td><a  id='Editar"+key+"' onclick='Editar(this)'><img src='../img/editar.png'></a></td> </tr>" );
+}
+}
+
+
+ 
   });
  
   $( "<table/>", {
@@ -38,16 +42,20 @@ function RedirigirHtml(){
 function Editar(img){
   //Cojo el abuelo del enlace donde tengo el evento onclick
   var padre= img.parentNode;
+
   var abuelo=padre.parentNode;
+  
  //listo los hijos que tiene  el abuelo
   var HijoAbuelo= document.getElementById(abuelo.id).childNodes;
    //listo los hijos que tiene  el segundo td que es donde esta el enlace
-  var hijoPadre=HijoAbuelo[1].childNodes;
+  var hijoPadre=HijoAbuelo[0].childNodes;
  //cojo el enlace
+alert(hijoPadre[1].href);
  var Enlace=hijoPadre[1].href;
+ 
+
  //luego pongo dicha ruta en el enlace donde esta el evento onlclick
  document.getElementById("Editar"+abuelo.id).href=Enlace;
-
 
 }
 function menu(){
