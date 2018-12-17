a4c858beafc1b76fa506347cd2ca7a9d32cdcd6d
Ruben-Vela-Aliaga <rubenvelaaliaga>
2018-12-17 17:47:44 +0100

    Muestra provincias,Terminado

--- ejemplo-opendata/public-html/ejemplo-opendata.js
+++ ejemplo-opendata/public-html/ejemplo-opendata.js
@@ -1,6 +1,6 @@
 
 let xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://mapas.valencia.es/lanzadera/opendata/OI-OBRAS-EJECUTADAS/JSON');
+xhr.open('GET', 'https://raw.githubusercontent.com/IagoLast/pselect/master/data/provincias.json');
 xhr.onload = function() {
     if (xhr.status === 200) {
         // Si todo ha ido bien
@@ -10,17 +10,18 @@ xhr.onload = function() {
         // ahora aun se trata de un texto, 
         // vamos a transformarla en objeto
         jsonObjeto = JSON.parse(jsonResponse);
-        console.log(jsonObjeto);
+       // console.log(jsonObjeto);
         // Veamoslo en consola si se quiere 
         //console.log(jsonObjeto.features);
 
         // Recorremos hora todos elementos de obras ejecutadas
-        jsonObjeto.features.forEach(element => {
+        
+        jsonObjeto.forEach(element => {
             // Le hacemos un console.log
-            //console.log(element);
+            console.log(element);
 
             // Obtenemos la tabla de obras
-            var tbl = document.getElementById("tablaObras");
+            var tbl = document.getElementById("tablaProvincias");
 
             // Creamos un tr
             var tr = document.createElement('tr');
@@ -29,26 +30,23 @@ xhr.onload = function() {
             var td = document.createElement('td');
 
             // Creamos un txt con el contenido obtenido
-            var txt = document.createTextNode(element.properties.titulo);
+            var txt = document.createTextNode(element.id);
             //console.log(element.properties.titulo);
-            var tdC = document.createElement('td');
-            var txtC = document.createTextNode(element.properties.contratista);
-            var tdP = document.createElement('td');
-            var txtP = document.createTextNode(element.properties.presupuesto);
+            var tdNombre = document.createElement('td');
+            var txtNombre = document.createTextNode(element.nm);
             // Anyadimos en cascada los elementos
             td.appendChild(txt);
             tr.appendChild(td);
-            tdC.appendChild(txtC);
-            tr.appendChild(tdC);
-            tdP.appendChild(txtP);
-            tr.appendChild(tdP);
+            tdNombre.appendChild(txtNombre);
+            tr.appendChild(tdNombre);
             tbl.appendChild(tr);
-
+  
         });
-        
+       
         }
     else {
         alert('Request failed.  Returned status of ' + xhr.status);
     }
+   
 };
 xhr.send();
